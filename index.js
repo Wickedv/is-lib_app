ddd355b2588650e6593f86916c48c8ac418e941e
manish <coish5insped@gmail.com>
2023-03-31 08:55:32 -0400

    basic changes to js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+let myLibrary = [];
 
+function Book(title,author,page,read) {
+    this.title=title;
+    this.author = author;
+    this.page = page;
+    this.read = read;
+}
+
+function addBookToLibrary(title,author,page,read) {
+  let newBook = new Book(title,author,page,read);
+  myLibrary.push(newBook);
+}
+
+let addNewBookBtn = document.getElementById("addBook")
+addNewBookBtn.addEventListener('click', function(){
+    let bookForm = document.querySelector("#book-form");
+    bookForm.style.display = "block";
+    alert("fuck off bithch");
+})
